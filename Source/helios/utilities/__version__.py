fa8435d6bf1cf6fe20314a56740a9d8d21ee10f8
Kip Warner <kip@thevertigo.com>
2022-02-25 22:10:38 -0800

    {PKG-INFO,debian/changelog,Source/helios/utilities/__version__.py}: Updated versioning metadata for impish...

--- Source/helios/utilities/__version__.py
+++ Source/helios/utilities/__version__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 #
 #   Helios, intelligent music.
-#   Copyright (C) 2015-2021 Cartesian Theatre. All rights reserved.
+#   Copyright (C) 2015-2022 Cartesian Theatre. All rights reserved.
 #
 
 # Utilities version...
 #  <https://www.python.org/dev/peps/pep-0440/>
-version = '0.5.20220109.post3'
+version = '0.5.20220225.post1'
